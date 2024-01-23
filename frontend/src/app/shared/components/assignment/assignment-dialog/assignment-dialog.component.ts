0841abe074bcfee792cb93a2b4f22a91b5e4d8d1
alarboulletmarin <andrea.larboullet-marin@outlook.com>
2024-01-23 21:04:42 +0100

    Update assignment dialog's functionalities and appearance
    This commit extends the functionality of the Assignment dialog by adding Subject, Professor, and Group services, as well as an edit mode. It also enhances the dialog's appearance, increasing its size and refining its layout with styles, and adjusts the structure and labels of form fields to improve the clarity of data input.

--- frontend/src/app/shared/components/assignment/assignment-dialog/assignment-dialog.component.ts
+++ frontend/src/app/shared/components/assignment/assignment-dialog/assignment-dialog.component.ts
@@ -5,6 +5,12 @@ import { catchError, throwError } from 'rxjs';
 import { Assignment } from '../../../../core/models/assignment.model';
 import { AuthService } from '../../../../core/services/auth/auth.service';
 import { AssignmentService } from '../../../../core/services/assignment/assignment.service';
+import { Subject } from '../../../../core/models/subject.model';
+import { Professor } from '../../../../core/models/professor.model';
+import { ProfessorService } from '../../../../core/services/professor/professor.service';
+import { SubjectService } from '../../../../core/services/subject/subject.service';
+import { GroupService } from '../../../../core/services/group/group.service';
+import { Group } from '../../../../core/models/group.model';
 
 @Component({
   selector: 'app-assignment-dialog',
@@ -16,16 +22,40 @@ export class AssignmentDialogComponent implements OnInit {
   public isEditMode: boolean = false;
   public tempAssignment!: Assignment;
 
+  public subjects: Subject[] = [];
+  public professors: Professor[] = [];
+  public groups: Group[] = [];
+  public selectedGroups: { [key: string]: boolean } = {};
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: Assignment,
     private dialogRef: MatDialogRef<AssignmentDialogComponent>,
     private authService: AuthService,
     private assignmentService: AssignmentService,
     private snackBar: MatSnackBar,
+    private professorService: ProfessorService,
+    private subjectService: SubjectService,
+    private groupService: GroupService,
   ) {}
 
   ngOnInit(): void {
     this.isProfessor = this.authService.hasRole('professor');
+    this.tempAssignment = { ...this.data };
+
+    this.subjectService.getSubjects().subscribe((subjects) => {
+      this.subjects = subjects;
+    });
+    this.professorService.getProfessors().subscribe((professors) => {
+      this.professors = professors;
+    });
+
+    this.groupService.getGroups().subscribe((groups) => {
+      this.groups = groups;
+      groups.forEach((group) => {
+        this.selectedGroups[group._id] =
+          this.tempAssignment?.group?.some((g) => g._id === group._id) ?? false;
+      });
+    });
   }
 
   toggleEditMode() {
@@ -37,6 +67,9 @@ export class AssignmentDialogComponent implements OnInit {
 
   saveChanges() {
     if (this.validateAssignment(this.tempAssignment)) {
+      this.tempAssignment.group = this.groups.filter(
+        (group) => this.selectedGroups[group._id],
+      );
       this.data = { ...this.tempAssignment };
       this.editAssignment();
       this.toggleEditMode();
